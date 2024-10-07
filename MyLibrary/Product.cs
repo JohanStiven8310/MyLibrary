688be4f9e57ae8ad3bc735259c4685953bf6c65b
JohanStiven8310 <jhanstivn93@gmail.com>
2024-10-07 09:26:59 -0500

    Creacion Clase "Product"

--- MyLibrary/Product.cs
+++ MyLibrary/Product.cs
@@ -8,11 +8,18 @@ namespace MyLibrary
 {
     public  class Product
     {
-
+        //Fiel, Campo, variable
+        private int Id_Field;
         public int Id;
+        {
+            get { return Id_Field; }
+            set { Id_Field = value; }
+
+        }
         public string Name;
         public decimal UnitPrice;
         public int UnitsInStock;
 
     }
 }
+
